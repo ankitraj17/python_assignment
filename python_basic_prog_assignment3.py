b89f6d8915c3f1bcdf6f7d75a80ff969718703ac
ankitraj17 <85974786+ankitraj17@users.noreply.github.com>
2022-07-23 16:03:01 -0700

    Add files via upload

---
+++ python_basic_prog_assignment3.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Programming_Assignment3
+
+# 1. Write a Python Program to Check if a Number is Positive, Negative or Zero?
+
+# In[9]:
+
+
+def checknumber():
+    num=int(input("enter the number "))
+    if num>0:
+        print("the number %.0f entered is positive "%num)
+    elif num==0:
+        print("the number %.0f entered is zero"%num)
+    else:
+        print("the number %.0f entered is negative"%num)
+checknumber()        
+
+
+# 2. Write a Python Program to Check if a Number is Odd or Even?
+
+# In[12]:
+
+
+def evenodd():
+    num=int(input("enter the number "))
+    if num%2==0:
+         print("the number %.0f entered is even "%num)
+    else:
+         print("the number %.0f entered is odd "%num)
+evenodd()        
+    
+
+
+# 3. Write a Python Program to Check Leap Year?
+
+# In[15]:
+
+
+def leapyear():
+    year=int(input("enter the year in yyyy format "))
+    if(year%400==0 and year%100==0):
+        print("the year %.0f entered is leap year "%year)
+    elif(year%4==0 and year%100!=0):
+        print("the year %.0f entered is leap year "%year)
+    else:
+        print("the year %.0f entered is non leap year "%year)
+leapyear()        
+
+
+# 4. Write a Python Program to Check Prime Number?
+
+# In[26]:
+
+
+def prime():
+    num=int(input("enter the number "))
+    if num>1:
+        for i in range(2,num):
+            if(num%i==0):
+                print("the number %.0f entered is non prime "%num)
+                break
+        else:
+            print("the number %.0f entered is prime "%num)
+            
+    else:
+        print("the number %.0f entered is non prime "%num)
+prime()        
+
+
+# 5. Write a Python Program to Print all Prime Numbers in an Interval of 1-10000?
+
+# In[33]:
+
+
+def prime():
+    for num in range(2,10000):
+        if num>1:
+            for i in range(2,num):
+                if(num%i==0):
+                    break
+            else:
+                print(num)
+prime()          
+
+
+# In[ ]:
+
+
+
+
